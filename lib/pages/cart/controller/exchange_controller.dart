86987b162f6db3e9cf8d2899a9b392cfe2c6a1e4
Tahaahm <tahaahmad837@gmail.com>
2024-03-05 22:47:04 +0300

    Update Some Feature

--- lib/pages/cart/controller/exchange_controller.dart
+++ lib/pages/cart/controller/exchange_controller.dart
@@ -15,7 +15,7 @@ class ExchangeRateController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    _timer = Timer.periodic(Duration(seconds: 25), (timer) {
+    _timer = Timer.periodic(Duration(seconds: 50), (timer) {
       fetchIqdAmount(false);
     });
   }
